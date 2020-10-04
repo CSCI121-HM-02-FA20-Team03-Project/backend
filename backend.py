989534008bfff069eb5da92ae8eb89592ecd9921
JarredAllen <jarredallen73@gmail.com>
2020-10-04 15:22:19 -0700

    Make things work

--- backend.py
+++ backend.py
@@ -5,17 +5,10 @@ app = Flask(__name__)
 
 @app.route('/')
 @app.route('/index')
+@app.route('/index.html')
 def index():
-    """A simple index which enables people to use the api"""
-    return '''
-    <!doctype html>
-    <title>Upload new File</title>
-    <h1>Upload new File</h1>
-    <form method=post enctype=multipart/form-data action="/api/ephemeral">
-      <input type=file name=image>
-      <input type=submit value=Upload>
-    </form>
-    '''
+    """Redirect to where they should go"""
+    redirect('/static/index.html')
 
 @app.route('/api/ephemeral', methods=['GET', 'POST'])
 def ephemeral():
@@ -26,6 +19,8 @@ def ephemeral():
             image = Image.open(f)
         except IOError:
             return "not a valid image", 400
+        except KeyError:
+            return "no image was provided; the files were {}.".format(str(list(request.files.keys()))), 400
         return get_model_output(image)
     else:
         return redirect('/')
